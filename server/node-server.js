8941ae18b1a22064787a64b869a6f5da22689732
Jessica Dash <167637926+jlbdash@users.noreply.github.com>
2024-07-20 20:38:05 -0400

    changes

--- server/node-server.js
+++ server/node-server.js
@@ -1,11 +1,38 @@
 import express from "express";
+import mysql from 'mysql';
 import { con, conn } from "./server-operator.js";
+import bodyParser from "body-parser";
 
 const port = 4000;
 
 const app = express();
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
+// Database for Users, Characters
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+
+  con.query("CREATE DATABASE if not exists test", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+});
+
+//Table for Users
+conn.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql =
+    "CREATE TABLE IF NOT EXISTS users (id INT(6) unsigned AUTO_INCREMENT PRIMARY KEY, username VARCHAR(60), 'character name' VARCHAR(25)";
+  conn.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+});
+
 
 // Database for Users, Characters
 con.connect(function (err) {
