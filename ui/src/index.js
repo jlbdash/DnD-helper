504036ba5177248bef33f466c3ec9458fdb6acdc
Jessica Dash <167637926+jlbdash@users.noreply.github.com>
2024-07-27 10:31:44 -0400

    changes

--- ui/src/index.js
+++ ui/src/index.js
@@ -7,11 +7,15 @@ import CreateCharacter from "./Addition/CreateCharacterApp.js";
 // loads the Game component with React
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <div style={{display: "flex"}}>
+  <div style={{ display: "flex" }}>
     <StrictMode>
-      <CreateCharacter />
-      <div style={{width:"50px"}}></div>
-      <SearchApp />
+      <div className="spaced">
+        <CreateCharacter />
+      </div>
+      <div style={{ width: "50px" }}></div>
+      <div className="spaced">
+        <SearchApp />
+      </div>
     </StrictMode>
   </div>
 );
