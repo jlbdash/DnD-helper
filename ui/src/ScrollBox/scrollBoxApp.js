7e04942c2acf79431550bf0f6641e859b2f0f6eb
jlbdash <167637926+jlbdash@users.noreply.github.com>
2024-09-10 13:41:42 -0400

    made component

--- ui/src/ScrollBox/scrollBoxApp.js
+++ ui/src/ScrollBox/scrollBoxApp.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import {searchMonster } from './apiInfo.js';
+import React, { useState } from 'react';
+import { searchMonster } from './apiInfo.js';
+import SearchButton from '../Components/searchButton.js';
 
 export default function ScrollBoxApp() {
+  const [searchText, setSearchText] = useState('');
+  const [isExact, setIsExact] = useState(false);
   const scrollBar = (
     <div className="monster">
       <h2>Monster Search</h2>
-      <input type="text" id="monsterInput" placeholder="Enter a monster name" />
-      <button type="submit" id="searchButton" onClick={searchMonster}>
-        {' '}
-        Search{' '}
-      </button>
+      <div style={{ flexDirection: 'row' }}>
+        <input
+          type="text"
+          id="monsterInput"
+          placeholder="Enter a monster name"
+        />
+        <button type="submit" id="searchButton" onClick={searchMonster}>
+          {' '}
+          Search{' '}
+        </button>{' '}
+        {/* A button to open the popup form */}{' '}
+        <button class="open-button" onclick="openForm()">
+          Open Form
+        </button>
+      </div>
       <section id="monsterResult"></section>
     </div>
   );
 
-  return (
-    <>
-      {scrollBar}
-    </>
-  );
+  return <>{scrollBar}</>;
 }
